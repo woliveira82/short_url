43d36d928249b6168b5167a125d2f9587dc0c4e1
woliveira82@gmail.com <woliveira82@gmail.com>
2021-07-03 22:57:32 -0300

    feat: basic flask project

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_pyfile('config.py')
+db = SQLAlchemy(app)
+
+from app.view import *
---
+++ config.py
@@ -0,0 +1,5 @@
+ENV = 'production'
+DEBUG = False
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/db_name'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
